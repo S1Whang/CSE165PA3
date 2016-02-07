35ae6c30e569d90a931cdcbf95eecb7ef6308d23
yumeijin <yuj019>
2016-02-07 00:20:40 -0800

    first commit

---
+++ Assets/RaceTrack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.IO;
+
+public class RaceTrack : MonoBehaviour {
+	int counter = 0;
+	string line;
+	public float explosionRadius = 30.0F;
+	float new_x;
+	float new_y;
+	float new_z;
+
+	// Use this for initialization
+	void Start () {
+		//Read the file and display it line by line
+		System.IO.StreamReader file = new System.IO.StreamReader ("XYZ file address");
+		while ((line = file.ReadLine ()) != null) {
+			Console.WriteLine (line);
+			string[] bits = line.Split(' ');
+			new_x = float.Parse (bits[0]);
+			new_y = float.Parse (bits[1]);
+			new_z = float.Parse (bits[2]);
+			transform.position.Set (new_x, new_y, new_z);
+			Gizmos.DrawWireSphere (transform.position, explosionRadius);
+			counter++;
+		}
+		file.Close ();
+		Console.ReadLine();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
